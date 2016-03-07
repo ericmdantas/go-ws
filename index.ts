17e25fd5f4516b702013d3dd00aa82ae5b1f2330
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-03-06 22:11:21 -0300

    .

--- index.ts
+++ index.ts
@@ -1,16 +1,39 @@
 /// <reference path="node_modules/angular2/typings/browser.d.ts" />
 
 import {bootstrap} from 'angular2/platform/browser';
-import {Component} from 'angular2/core';
+import {
+  Component,
+  OnInit
+} from 'angular2/core';
 
 @Component({
   selector: 'chat',
   template: `
     <h1>chat on</h1>
+    <input type="text" [(ngModel)]="msg" (keyup.enter)="go(msg)"/>
+    <button type="submit" (submit)="go(msg)">go</button>
+
+    <div>
+      <ul>
+        <li *ngFor="#m of msgs">{{m}}</li>
+      </ul>
+    </div>
   `
 })
-class Chat {
+class Chat implements OnInit {
+  msg: string = 'yo';
+  msgs: string[] = [];
+  ws: WebSocket;
 
+  ngOnInit() {
+    this.ws = new WebSocket('ws://localhost:3333');
+  }
+
+  go(msg: string) {
+    console.log(msg);
+
+    this.ws.send(msg);
+  }
 }
 
 bootstrap(Chat);
--- main.go
+++ main.go
@@ -1,7 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+
+	ws "github.com/gorilla/websocket"
+)
+
+func initWs() {
+	fmt.Println("ws on")
+
+	u := url.URL{Scheme: "ws", Host: "localhost:3333", Path: "/"}
+
+	c, _, err := ws.DefaultDialer.Dial(u.String(), nil)
+
+	if err != nil {
+		log.Fatal("dial error:", err)
+	}
+
+	defer c.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		defer c.Close()
+		defer close(done)
+
+		for {
+			_, message, err := c.ReadMessage()
+
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+
+			log.Println("recv: %s", message)
+		}
+	}()
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case t := <-ticker.C:
+			err := c.WriteMessage(ws.TextMessage, []byte(t.String()))
+
+			if err != nil {
+				log.Println("write:", err)
+				return
+			}
+		}
+	}
+}
+
+func initHTTP() {
+	fmt.Println("http on")
+
+	http.Handle("/", http.FileServer(http.Dir("./")))
+	log.Fatal(http.ListenAndServe(":3333", nil))
+}
 
 func main() {
-	fmt.Println("yo!")
+	initWs()
+	initHTTP()
 }
