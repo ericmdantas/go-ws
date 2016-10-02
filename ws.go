ef455b2e1a1d303fdc49726b7bdb5ccfd5579cca
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-10-02 20:55:36 -0300

    .

--- ws.go
+++ ws.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"golang.org/x/net/websocket"
 )
 
+type msg struct {
+	Type string `json:"type"`
+	Data struct {
+		Msg string `json:"msg"`
+	} `json:"data"`
+}
+
 func main() {
 	http.Handle("/", http.FileServer(http.Dir(".")))
 
 	http.Handle("/echo", websocket.Handler(func(conn *websocket.Conn) {
-		conn.Write([]byte("yo!"))
+		for {
+			var b []byte
+			var m msg
+
+			websocket.Message.Receive(conn, &b)
+
+			json.Unmarshal(b, &m)
+
+			fmt.Println(m)
+
+			bm, _ := json.Marshal(m)
+
+			conn.Write(bm)
+		}
 	}))
 
 	http.ListenAndServe(":1234", nil)
